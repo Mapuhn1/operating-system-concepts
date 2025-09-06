9ed7b090f951cb044fd8418622600fdb0f49338a
Hazel <mapuhn1@student.op.ac.nz>
2025-09-06 20:13:14 +1200

    set source for user script

--- assignment1/task1/user_setup.sh
+++ assignment1/task1/user_setup.sh
@@ -7,6 +7,8 @@ LOGFILE="user_setup_$(date +%Y%m%d_%H%M%S).log"
 
 source "$(dirname "$0")/logging.sh"
 source "$(dirname "$0")/input.sh"
+source "$(dirname "$0")/user.sh"
+
 check_input_file
 tail -n +2 "$INPUT_FILE" | while IFS=',' read -r email birthDate groups sharedFolder; do
 
