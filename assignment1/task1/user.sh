85ca97b0dd73294789f0be4d3ccf0cc41436cf7f
Hazel <mapuhn1@student.op.ac.nz>
2025-09-06 20:20:26 +1200

    create function for generating passwords

--- assignment1/task1/user.sh
+++ assignment1/task1/user.sh
@@ -6,3 +6,9 @@ generate_username() {
     local surname=$(echo "$email" | cut -d'.' -f2 | cut -d'@' -f1)
     echo "$(echo "${surname:0:1}")$(tr '[:lower:]' '[:upper:]' <<< ${firstname:0:1})${firstname:1}"
 }
+
+generate_password() {
+    local birth="$1"
+    echo "$(echo "$birth" | cut -d'/' -f1)$(echo "$birth" | cut -d'/' -f2)"
+}
+
