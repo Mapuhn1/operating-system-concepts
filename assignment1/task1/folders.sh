675c017ac01c35a3aa7a9f6d2245dd3c2242231e
Hazel <mapuhn1@student.op.ac.nz>
2025-09-07 15:43:57 +1200

    create folder, set permissions, ownership and log

--- assignment1/task1/folders.sh
+++ assignment1/task1/folders.sh
@@ -6,6 +6,9 @@ setup_shared_folder(){
     [[ -z "$folder" ]] && return
 
     if [[ ! -d "$folder" ]]; then
-
+        sudo mkdir -p "$folder"
+        sudo chmod 770 "$folder"
+        sudo chown root:root "$folder"
+        log "Created shared folder $folder"
     fi
 }
