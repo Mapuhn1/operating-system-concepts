9e00025475167bd28ca17b4d3890977ae63dd5ac
Hazel <mapuhn1@student.op.ac.nz>
2025-09-09 09:45:27 +1200

    log to the log file on alias creation

--- assignment1/task1/aliases.sh
+++ assignment1/task1/aliases.sh
@@ -5,4 +5,7 @@ setup_aliases() {
     if groups "$username" | grep -q "sudo"; then
         echo "alias myls='ls -al'" | sudo tee -a "/home/$username/.bash_alias" >/dev/null
         sudo chown "$username:$username" "/home/$username/.bash_alias"
+
+        log "Added alias myls for $username"
+    fi
 }
