0c6b69c9b1256fd28edad6d3d668105a7ef87bb3
Hazel <mapuhn1@student.op.ac.nz>
2025-09-09 09:44:51 +1200

    ensure the .bash_aliases file is owned by the user not root

--- assignment1/task1/aliases.sh
+++ assignment1/task1/aliases.sh
@@ -4,5 +4,5 @@ setup_aliases() {
     local username="$1"
     if groups "$username" | grep -q "sudo"; then
         echo "alias myls='ls -al'" | sudo tee -a "/home/$username/.bash_alias" >/dev/null
-        
+        sudo chown "$username:$username" "/home/$username/.bash_alias"
 }
