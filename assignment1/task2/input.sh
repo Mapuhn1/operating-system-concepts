2b1ee4daf842786d3bd90e8aa0e2d1a43e2fa50c
Hazel <mapuhn1@student.op.ac.nz>
2025-09-10 17:02:03 +1200

    check if input is a directory and if not log to logfile then exit with an error

--- assignment1/task2/input.sh
+++ assignment1/task2/input.sh
@@ -4,4 +4,8 @@ check_backup_directory() {
     if [[ -z "$BACKUP_DIR" ]]; then
         read -p "Enter directory path to backup: " BACKUP_DIR
     fi
-}
+    
+    if [[ ! -d "$BACKUP_DIR" ]]; then
+        log "ERROR: Directory '$BACKUP_DIR' does not exist"
+        exit 1
+    fi
