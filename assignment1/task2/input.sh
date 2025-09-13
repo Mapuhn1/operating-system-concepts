41a17d9f7a2e9af2b70a85679344b76d5d99a1b0
Hazel <mapuhn1@student.op.ac.nz>
2025-09-13 15:33:28 +1200

    setup shell variables for path, name and archive name

--- assignment1/task2/input.sh
+++ assignment1/task2/input.sh
@@ -9,3 +9,9 @@ check_backup_directory() {
         log "ERROR: Directory '$BACKUP_DIR' does not exist"
         exit 1
     fi
+    
+    BACKUP_DIR=$(realpath "$BACKUP_DIR")
+    BACKUP_NAME=$(basename "$BACKUP_DIR")
+    ARCHIVE_NAME="${BACKUP_NAME}.tar.gz"
+    
+}
