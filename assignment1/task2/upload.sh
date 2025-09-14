4937266c3713f9a3bfa053ac06cfda33cd49be64
Hazel <mapuhn1@student.op.ac.nz>
2025-09-15 11:09:33 +1200

    if scp command fails terminate process with exit 1

--- assignment1/task2/upload.sh
+++ assignment1/task2/upload.sh
@@ -2,4 +2,11 @@
 
 upload_backup() {
     log "Uploading to $REMOTE_USER@$REMOTE_HOST:$REMOTE_DIR"
+    
+    if ! scp -P "$REMOTE_PORT" "$ARCHIVE_NAME" "$REMOTE_USER@$REMOTE_HOST:$REMOTE_DIR/"; then
+        log "ERROR: Upload failed"
+        exit 1
+    fi
+    
+    log "Upload completed successfully"
 }
