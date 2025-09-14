dbea590de7c69fa73faf343a1c0c18c749a44de3
Hazel <mapuhn1@student.op.ac.nz>
2025-09-14 20:38:04 +1200

    else log error and terminate process with exit 1 for error

--- assignment1/task2/backup.sh
+++ assignment1/task2/backup.sh
@@ -5,5 +5,8 @@ create_backup() {
 
     if tar -czf "$ARCHIVE_NAME" -C "$(dirname "$BACKUP_DIR")" "$(basename "$BACKUP_DIR")"; then
         log "Archive created: $ARCHIVE_NAME"
+    else
+        log "ERROR: Failed to create archive"
+        exit 1
     fi
 }
