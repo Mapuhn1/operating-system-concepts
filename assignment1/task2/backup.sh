888a63974732905eaa7e0b778430ebd870039518
Hazel <mapuhn1@student.op.ac.nz>
2025-09-16 11:18:09 +1200

    add code comments for backup module

--- assignment1/task2/backup.sh
+++ assignment1/task2/backup.sh
@@ -1,8 +1,10 @@
 #backup.sh
+# Creates a compressed archive of the backup directory
 
 create_backup() {
     log "Creating archive: $ARCHIVE_NAME"
 
+    # Compress the backup directory into a .tar.gz archive
     if tar -czf "$ARCHIVE_NAME" -C "$(dirname "$BACKUP_DIR")" "$(basename "$BACKUP_DIR")"; then
         log "Archive created: $ARCHIVE_NAME"
     else
